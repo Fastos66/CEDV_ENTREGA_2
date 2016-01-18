bb8a71b4ea2bb81291cbf529228910045a1b7683
Javi Mendo <reyruchowow@gmail.com>
2016-01-18 13:21:42 +0100

    Codigo para despues pedir el nombre para introducir en el ranking mas algunas imagenes y archivos para gimp

--- Pacman/include/MyScenePlay.h
+++ Pacman/include/MyScenePlay.h
@@ -2,14 +2,18 @@
 #define MyScenePlay_H
 #include <Ogre.h>
 #include <OIS/OIS.h>
+#include <CEGUI.h>
+#include <RendererModules/Ogre/Renderer.h>
 class MyScenePlay
 {
 	public:
 		MyScenePlay(Ogre::SceneManager* sceneManager);
 		~MyScenePlay();
 		void cargarscenainicial();
+		void codigoParapedirelnombreFUTURO();
 	private:
-		Ogre::SceneManager* _sceneManager;	
+		Ogre::SceneManager* _sceneManager;
+		CEGUI::Window* _sheet;	
 };
 
 
--- Pacman/src/MyScenePlay.cpp
+++ Pacman/src/MyScenePlay.cpp
@@ -3,9 +3,34 @@
 #include "MyScenePlay.h"
 MyScenePlay::MyScenePlay(Ogre::SceneManager* sceneManager){
 	sceneManager=_sceneManager;
+	_sheet = CEGUI::System::getSingleton().getDefaultGUIContext().getRootWindow();
 }
 
 MyScenePlay::~MyScenePlay() {}
 
 void MyScenePlay::cargarscenainicial(){
+
 }	
+
+void MyScenePlay::codigoParapedirelnombreFUTURO(){
+	CEGUI::Window* ventpuntos = CEGUI::WindowManager::getSingleton().loadLayoutFromFile("Puntuacion.layout");
+	ventpuntos->setPosition(CEGUI::UVector2(CEGUI::UDim(0.21f,0),CEGUI::UDim(0.20f,0)));
+	CEGUI::Window* imglogo = CEGUI::WindowManager::getSingleton().createWindow("TaharezLook/StaticImage","VentImagenTop");
+  	CEGUI::ImageManager::getSingleton().addFromImageFile("ImagenLOGOTop","puntuaciontop.png");
+  	imglogo -> setProperty("Image","ImagenLOGOTop");
+  	imglogo -> setProperty("BackgroundEnabled","False");
+  	imglogo -> setProperty("FrameEnabled","False");
+  	imglogo -> setPosition(CEGUI::UVector2(CEGUI::UDim(0.42f,0),CEGUI::UDim(0.13f,0)));
+  	imglogo -> setSize(CEGUI::USize(CEGUI::UDim(0.12f,0),CEGUI::UDim(0.12f,0)));  
+  	ventpuntos -> addChild(imglogo);
+
+  	CEGUI::Window* imgtexto = CEGUI::WindowManager::getSingleton().createWindow("TaharezLook/StaticImage","VentImagenOnRanking");
+  	CEGUI::ImageManager::getSingleton().addFromImageFile("ImagenOR","onranking.png");
+  	imgtexto -> setProperty("Image","ImagenOR");
+  	imgtexto -> setProperty("BackgroundEnabled","False");
+  	imgtexto -> setProperty("FrameEnabled","False");
+  	imgtexto -> setPosition(CEGUI::UVector2(CEGUI::UDim(0.08f,0),CEGUI::UDim(0.07f,0)));
+  	imgtexto -> setSize(CEGUI::USize(CEGUI::UDim(0.24f,0),CEGUI::UDim(0.16f,0)));  
+  	ventpuntos -> addChild(imgtexto);
+  	_sheet-> addChild(ventpuntos);
+}
